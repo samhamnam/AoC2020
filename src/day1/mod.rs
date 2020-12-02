9543ce8c96686c43e7713cfe117344d5c47367b3
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2020-12-02 15:07:02 +0100

    Finished Day 1 and 2.

---
+++ src/day1/mod.rs
@@ -0,0 +1,43 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+#[allow(dead_code)]
+pub fn run() {
+    println!("-------- Day 1 --------");
+    println!("PART 1:");
+
+    let input = {
+        let mut file = File::open("src/day1/input.txt").expect("Failed to open input!");
+        let mut content = String::new();
+        file.read_to_string(&mut content).expect("Failed to read!");
+
+        let strings = content.split("\n").collect::<Vec<&str>>().clone();
+        let mut numbers = Vec::new();
+        for s in strings {
+            numbers.push(s.parse::<i64>().expect("Failed to parse"))
+        }
+        numbers
+    };
+    for i in 0..input.len() {
+        for j in 0..input.len() {
+            let val = input[i] + input[j];
+            let pro = input[i] * input[j];
+            if val == 2020 {
+                println!("{} + {}: {}", input[i], input[j], pro);
+            }
+        }
+    }
+
+    println!("\n PART 2:");
+    for i in 0..input.len() {
+        for j in 0..input.len() {
+            for k in 0..input.len() {
+                let val = input[i] + input[j] + input[k];
+                if val == 2020 {
+                    let pro = input[i] * input[j] * input[k];
+                    println!("{} + {} + {}: {}", input[i], input[j], input[k], pro);
+                }
+            }
+        }
+    }
+}
---
+++ src/day2/mod.rs
@@ -0,0 +1,88 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+#[derive(Clone)]
+struct Password {
+    pub min: u32,
+    pub max: u32,
+    pub letter: String,
+    pub pass: String,
+}
+
+#[allow(dead_code)]
+pub fn run() {
+    let passwords = {
+        let mut file = File::open("src/day2/input.txt").expect("Failed to open input!");
+        let mut file_string = String::new();
+        file.read_to_string(&mut file_string)
+            .expect("Failed to read");
+        let rows: Vec<&str> = file_string.split("\n").collect();
+
+        let mut passwords = Vec::new();
+
+        for r in rows {
+            let parts: Vec<&str> = r.split(" ").collect();
+
+            let (min, max) = {
+                let text: Vec<&str> = parts[0].split("-").collect();
+                (
+                    text[0].parse::<u32>().expect("Failed to parse"),
+                    text[1].parse::<u32>().expect("Failed to parse"),
+                )
+            };
+            let letter = parts[1].split(":").collect::<Vec<&str>>()[0].to_owned();
+            let pass = parts[2].to_owned();
+
+            passwords.push(Password {
+                min,
+                max,
+                letter,
+                pass,
+            });
+        }
+        passwords
+    };
+
+    println!("-------- Day 2 --------");
+    println!("PART 1:");
+    let mut total_amount = 0;
+    for p in passwords.clone() {
+        let split: Vec<&str> = p.pass.split("").collect();
+        let mut amount = 0;
+        for l in split {
+            if l == p.letter {
+                amount += 1;
+            }
+        }
+
+        if amount >= p.min && amount <= p.max {
+            total_amount += 1;
+        }
+    }
+    println!("Valid passwords: {}", total_amount);
+
+    println!("\n\nPART 2:");
+    let mut total_amount = 0;
+    for p in passwords {
+        let split: Vec<&str> = p.pass.split("").collect();
+
+        let min = (p.min) as usize;
+        let max = (p.max) as usize;
+
+        let l1 = split[min];
+        let l2 = split[max];
+
+        if l1 == p.letter && l2 != p.letter || l2 == p.letter && l1 != p.letter {
+            println!("{}-{} {}: {}", p.min, p.max, p.letter, p.pass);
+            println!(
+                "{} {} | {} {}\n",
+                l1,
+                l2,
+                l1 == p.letter && l2 != p.letter,
+                l2 == p.letter && l1 != p.letter
+            );
+            total_amount += 1;
+        }
+    }
+    println!("Valid passwords: {}", total_amount);
+}
