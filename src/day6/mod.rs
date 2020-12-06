542c9085c82660ac831e95d3e7fa8a1685611f14
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2020-12-06 11:19:46 +0100

    Finished day 6.

--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -2,6 +2,8 @@ use std::fs::File;
 use std::io::prelude::*;
 
 pub fn run() {
+    println!("-------- Day 6 --------");
+
     let groups = {
         let mut f = File::open("src/day6/input.txt").expect("Failed to open input!");
         let mut s = String::new();
@@ -10,7 +12,46 @@ pub fn run() {
         create_groups(s)
     };
 
-    let clean: Vec<String> = groups.iter().map(|s| remove_dupes(s.to_string())).collect();
+    part_one(groups.clone());
+    part_two(groups);
+}
+
+fn part_two(groups: Vec<String>) {
+    let groups: Vec<Vec<&str>> = groups.iter().map(|s| s.split("\n").collect()).collect();
+
+    let mut count = 0;
+    for g in groups.clone() {
+        let mut checked_chars = String::new();
+        for i in 0..g.len() {
+            for c in g[i].chars() {
+                if !checked_chars.contains(c) {
+                    let mut vote = true;
+                    for j in 0..g.len() {
+                        if j == i {
+                            continue;
+                        }
+                        vote = vote && g[j].contains(c);
+                    }
+
+                    if vote {
+                        count += 1;
+                        checked_chars.push(c);
+                    }
+                }
+            }
+        }
+    }
+
+    println!("Part 2: Vote count: {:?}\n", count);
+}
+
+fn part_one(groups: Vec<String>) {
+    let groups: Vec<Vec<&str>> = groups.iter().map(|s| s.split("\n").collect()).collect();
+    let groups: Vec<String> = groups
+        .iter()
+        .map(|v| v.iter().map(|s| s.to_owned()).collect())
+        .collect();
+    let clean: Vec<String> = groups.iter().map(|s| remove_dupes(s.clone())).collect();
     let count = {
         let mut count = 0;
         for s in clean {
@@ -23,11 +64,7 @@ pub fn run() {
 
 fn create_groups(s: String) -> Vec<String> {
     let groups: Vec<&str> = s.split("\n\n").collect();
-    let groups: Vec<Vec<&str>> = groups.iter().map(|s| s.split("\n").collect()).collect();
-    let groups: Vec<String> = groups
-        .iter()
-        .map(|v| v.iter().map(|s| s.to_string()).collect())
-        .collect();
+    let groups: Vec<String> = groups.iter().map(|s| s.to_string()).collect();
     groups
 }
 
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ mod day5;
 mod day6;
 
 fn main() {
-    /*day1::run();
+    day1::run();
     day2::run();
     day3::run();
     day4::run();
-    day5::run();*/
+    day5::run();
     day6::run();
 }
